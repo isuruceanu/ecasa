4bdb3437a7b54c64944362e14550349d3b07a8d7
isuruceanu <surucion@gmail.com>
2015-01-23 00:04:40 +0200

    added websocket

--- src/ecasa.web/main.go
+++ src/ecasa.web/main.go
@@ -5,15 +5,23 @@ import(
 	"github.com/martini-contrib/render"
 )
 
+
+
 func main() {
 	m := martini.Classic()
+		
 	m.Use(render.Renderer(render.Options{
 		Layout: "layout",
 	}))
 	
+	go h.run()
+	
 	m.Get("/", func(r render.Render) {
-		r.HTML(200, "dashboard", nil)
+		
+		r.HTML(200, "dashboard", "21")
 	})
+	
+	m.Get("/ws", wsHandler)
 	
 	m.Group("/devices", DeviceController)
 
