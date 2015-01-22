0cd7429043e6a2f35612f1b5df7514c8e7b43234
isuruceanu <surucion@gmail.com>
2015-01-22 21:26:28 +0200

    separate handlers into own files: controllers

--- src/ecasa.web/main.go
+++ src/ecasa.web/main.go
@@ -11,20 +11,11 @@ func main() {
 		Layout: "layout",
 	}))
 	
-		
-	devices := Devices{
-		Device{Id:1, Name:"Device1"},
-		Device{Id:2, Name:"Device 2"},
-		Device{Id:4, Name:"Device 4"},
-	}
-	
 	m.Get("/", func(r render.Render) {
 		r.HTML(200, "dashboard", nil)
 	})
 	
-	m.Get("/devices", func(r render.Render){
-		r.HTML(200, "devices", devices)
-	})
-	
+	m.Group("/devices", DeviceController)
+
 	m.Run()
 }
