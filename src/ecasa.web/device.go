207e5a3f5a6202926b563a7d6c49b8cccc9cf86f
isuruceanu <surucion@gmail.com>
2015-02-16 19:45:09 +0200

    update to latest code

--- src/ecasa.web/device.go
+++ src/ecasa.web/device.go
@@ -1,30 +1,65 @@
 package main
 
-import(
+import (
+	"fmt"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
+	"strconv"
 )
 
-type Device struct{
-	Name 		string		`json:name`
-	Id			int			`json:id`
+type device struct {
+	Name   string `json:name`
+	Id     int    `json:id`
+	IpAddr string `json:ipaddr`
 }
 
-type Devices []Device
+var devices = make([]device, 0)
+var id = 0
 
-
-
-func DeviceController(r martini.Router){
-	r.Get("/", Index)
+func getNextId() int {
+	id += 1
+	return id
 }
 
-func Index(r render.Render){
+func getDeviceById(id int) (device, int) {
+	for i, d := range devices {
+		if d.Id == id {
+			return d, i
+		}
+	}
+	return device{}, -1
+}
 
-	devices := Devices{
-		Device{Id:1, Name:"Device1"},
-		Device{Id:2, Name:"Device 2"},
-		Device{Id:4, Name:"Device 4"},
+func DeviceController(r martini.Router) {
+	r.Get("/", index)
+	r.Post("/", add)
+	r.Get("/:id", edit)
+	r.Delete("/:id", removeDevice)
+}
+
+func add(r render.Render) {
+
+}
+
+func edit(r render.Render) {
+
+}
+
+func removeDevice(params martini.Params) {
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		fmt.Sprintf("can not convert to int %s", params["id"])
 	}
 
+	_, idx := getDeviceById(id)
+	if idx < 0 {
+		//throw exception here
+	}
+
+	devices = append(devices[:idx], devices[idx+1:]...)
+}
+
+func index(r render.Render) {
 	r.HTML(200, "devices", devices)
 }
